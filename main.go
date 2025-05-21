9dd742844540e1d9595dabd4c3a4e56070f4d815
Andrew Hollis <thejester129@gmail.com>
2025-05-21 21:09:38 +0100

    add keys to routes

--- main.go
+++ main.go
@@ -7,39 +7,53 @@ import (
 )
 
 type JsonItem map[string]any
+type Items map[string]JsonItem
 
-var item JsonItem
+var items = make(Items, 0)
 
 func main() {
 	router := gin.Default()
-	router.GET("/items", getItems)
-	router.PUT("/items", putItem)
-	router.PATCH("/items", patchItem)
+	router.GET("/", getItems)
+	router.GET("/:key", getByKey)
+	router.PUT("/:key", putItem)
+	router.PATCH("/:key", patchItem)
 
 	router.Run("localhost:8080")
 }
 
 func getItems(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, item)
+	c.IndentedJSON(http.StatusOK, items)
+}
+
+func getByKey(c *gin.Context) {
+	key := c.Param("key")
+
+	c.IndentedJSON(http.StatusOK, items[key])
 }
 
 func putItem(c *gin.Context) {
+	key := c.Param("key")
+
 	var newItem JsonItem
 
 	if err := c.BindJSON(&newItem); err != nil {
 		return
 	}
 
-	item = newItem
+	items[key] = newItem
 	c.IndentedJSON(http.StatusCreated, newItem)
 }
 
 func patchItem(c *gin.Context) {
+	key := c.Param("key")
+
 	var patchItem JsonItem
 
 	if err := c.BindJSON(&patchItem); err != nil {
 		return
 	}
+
+	item := items[key]
 
 	for k, v := range patchItem {
 		item[k] = v
